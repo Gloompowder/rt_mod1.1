869eb80d538b836d86a269f6451ca71e8a0728c6
William <willlin1996@gmail.com>
2020-03-30 17:27:26 -0400

    second save

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
     has_many :receipts 
-    has_many :stores, through :receipts 
-    has_many :shoppers, through :receipts
+    has_many :stores, through: :receipts 
+    has_many :shoppers, through: :receipts
 end
--- app/models/shopper.rb
+++ app/models/shopper.rb
@@ -1,5 +1,8 @@
 class Shopper < ActiveRecord::Base
     has_many :receipts 
-    has_many :products, through :receipts 
-    has_many :stores, through :receipts 
+    has_many :products, through: :receipts 
+    has_many :stores, through: :receipts 
+
+    # which products did this shopper buy?
+
 end
